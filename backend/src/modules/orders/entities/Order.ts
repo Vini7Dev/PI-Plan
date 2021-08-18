a8f4fd1fd33b2782c7e148542333079b10f647a0
Vinícius Gabriel <68403363+Vinicius7Dev@users.noreply.github.com>
2021-08-18 11:45:06 -0300

    Creation of order entity

---
+++ backend/src/modules/orders/entities/Order.ts
@@ -0,0 +1,80 @@
+import {
+  Column,
+  CreateDateColumn,
+  DeleteDateColumn,
+  Entity,
+  PrimaryColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+// Classe que contém os dados dos pedidos salvos no banco de dados
+@Entity('order')
+class Order {
+  @PrimaryColumn('uuid')
+  id: string;
+
+  @Column('uuid')
+  customer_id: string;
+
+  @Column('int')
+  current_status: number;
+
+  @Column('int')
+  current_proccess: number;
+
+  @Column()
+  title: string;
+
+  @Column()
+  description: string;
+
+  @Column()
+  cep: string;
+
+  @Column()
+  street: string;
+
+  @Column('int')
+  number: number;
+
+  @Column()
+  complement: string;
+
+  @Column()
+  district: string;
+
+  @Column()
+  city: string;
+
+  @Column()
+  uf: string;
+
+  @Column()
+  country: string;
+
+  @Column()
+  installation_environments: string;
+
+  @Column()
+  mobile_delivery_forecast: string;
+
+  @Column()
+  payment_method: string;
+
+  @Column('number')
+  net_value: number;
+
+  @Column('number')
+  expenses_value: number;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+
+  @DeleteDateColumn()
+  deleted_at: Date;
+}
+
+export default Order;
