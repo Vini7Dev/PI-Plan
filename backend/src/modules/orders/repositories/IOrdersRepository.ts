8c688c8ab10dd62b3cb038685caeb9cbe57831c5
Vinícius Gabriel <68403363+Vinicius7Dev@users.noreply.github.com>
2021-08-20 10:36:25 -0300

    Creation of orders repository interface and create order dto

---
+++ backend/src/modules/orders/repositories/IOrdersRepository.ts
@@ -0,0 +1,11 @@
+import ICreateOrderDTO from '../dtos/ICreateOrderDTO';
+import Order from '../typeorm/entities/Order';
+
+interface IOrdersRepository {
+  list(): Promise<Order[]>;
+  create(data: ICreateOrderDTO): Promise<Order>;
+  update(data: ICreateOrderDTO): Promise<Order>;
+  delete(): Promise<string>;
+}
+
+export default IOrdersRepository;
