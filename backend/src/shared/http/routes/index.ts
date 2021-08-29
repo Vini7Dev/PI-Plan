be01ba4c8e0135ce67ed705085b539747ba67d4d
Vinícius Gabriel <viniciusgabriel.dev@gmail.com>
2021-08-28 21:43:39 -0300

    Apply reminders routes into server

--- backend/src/shared/http/routes/index.ts
+++ backend/src/shared/http/routes/index.ts
@@ -6,6 +6,7 @@ import sectionRoutes from '../../../modules/users/routes/section.routes';
 import customerRoutes from '../../../modules/customers/routes/customer.routes';
 import orderRoutes from '../../../modules/orders/routes/order.routes';
 import installationRoutes from '../../../modules/installations/routes/installation.routes';
+import reminderRoutes from '../../../modules/users/routes/reminder.routes';
 
 // Instanciando o objeto de rotas principal
 const routes = Router();
@@ -17,5 +18,6 @@ routes.use('/sections', sectionRoutes);
 routes.use('/customers', customerRoutes);
 routes.use('/orders', orderRoutes);
 routes.use('/installations', installationRoutes);
+routes.use('/reminders', reminderRoutes);
 
 export default routes;
