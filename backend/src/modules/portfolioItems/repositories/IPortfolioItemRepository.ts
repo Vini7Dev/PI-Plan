3c8cdbc0395818a8e37330831496e0289d8a73d0
Vinícius Gabriel <viniciusgabriel.dev@gmail.com>
2021-09-06 15:54:02 -0300

    Implementation of PortfolioItemsRepository

--- backend/src/modules/portfolioItems/repositories/IPortfolioItemRepository.ts
+++ backend/src/modules/portfolioItems/repositories/IPortfolioItemRepository.ts
@@ -1,11 +1,12 @@
 import ICreatePortfolioItemDTO from '../dtos/ICreatePortfolioItemDTO';
 import PortfolioItem from '../typeorm/entities/PortfolioItem';
 
-interface IPortfolioRepository {
-  list(): Promise<PortfolioItem>;
+interface PortfolioItemRepository {
+  findById(id: string): Promise<PortfolioItem | undefined>;
+  list(): Promise<PortfolioItem[]>;
   create(data: ICreatePortfolioItemDTO): Promise<PortfolioItem>;
   update(data: ICreatePortfolioItemDTO): Promise<PortfolioItem>;
   delete(id: string): Promise<string>;
 }
 
-export default IPortfolioRepository;
+export default PortfolioItemRepository;
