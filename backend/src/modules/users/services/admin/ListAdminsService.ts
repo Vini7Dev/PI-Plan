a164ede83fce630c71edf36056ad7554bcc5545b
Vinícius Gabriel <viniciusgabriel.dev@gmail.com>
2021-09-06 14:51:11 -0300

    Delet of class transformer

--- backend/src/modules/users/services/admin/ListAdminsService.ts
+++ backend/src/modules/users/services/admin/ListAdminsService.ts
@@ -2,7 +2,6 @@ import { inject, injectable } from 'tsyringe';
 
 import Admin from '../../typeorm/entities/Admin';
 import IAdminsRepository from '../../repositories/IAdminsRepository';
-import IClassTransformer from '../../../../shared/container/providers/ClassTransformerProvider/models/IClassTransformer';
 
 @injectable()
 class ListAdminsService {
@@ -10,22 +9,13 @@ class ListAdminsService {
     // Repositório dos administradores
     @inject('AdminsRepository')
     private adminsRepository: IAdminsRepository,
-
-    @inject('ClassTransformer')
-    private classTransformer: IClassTransformer,
   ) {}
 
   // Serviço para listar os administradores cadastrados
   public async execute(): Promise<Admin[]> {
     const adminsList = await this.adminsRepository.list();
 
-    const adminsClassTransformed = adminsList.map((admin) => {
-      const adminWithoutPassword = this.classTransformer.deleteProps(admin, ['password']);
-
-      return adminWithoutPassword;
-    });
-
-    return adminsClassTransformed;
+    return adminsList;
   }
 }
 
--- backend/src/shared/container/providers/index.ts
+++ backend/src/shared/container/providers/index.ts
@@ -5,8 +5,6 @@ import DateProvider from './DateProvider/implementations/DateProvider';
 
 import IHashProvider from './HashProvider/models/IHashProvider';
 import BCryptProvider from './HashProvider/implementations/BCryptProvider';
-import IClassTransformer from './ClassTransformerProvider/models/IClassTransformer';
-import ClassTransformer from './ClassTransformerProvider/implementations/ClassTransformer';
 
 // Registrando os containers dos provedores
 container.registerSingleton<IDateProvider>(
@@ -15,8 +13,4 @@ container.registerSingleton<IDateProvider>(
 
 container.registerSingleton<IHashProvider>(
   'HashProvider', BCryptProvider,
-);
-
-container.registerSingleton<IClassTransformer>(
-  'ClassTransformer', ClassTransformer,
 );
