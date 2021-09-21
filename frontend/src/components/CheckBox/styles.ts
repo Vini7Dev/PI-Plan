1557cda3fbac06b994b453aa5e066ba66fff9384
Vinícius Gabriel <viniciusgabriel.dev@gmail.com>
2021-09-21 16:44:54 -0300

    .

--- frontend/src/components/CheckBox/styles.ts
+++ frontend/src/components/CheckBox/styles.ts
@@ -1,16 +1,22 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
+  display: flex;
+  align-items: center;
   margin: 20px 0;
   width: 100%;
 
   label{
     font-size: 20px;
     font-family: Arial, Helvetica, sans-serif;
-    color: #ceaa7b;
     font-weight: bold;
     color: #36373D;
     margin-right: 20px;
+  }
+
+  input {
+    width: 20px;
+    height: 20px;
   }
 
   div p {
