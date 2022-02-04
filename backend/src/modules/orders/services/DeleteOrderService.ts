eb9f52264f1a1edf411f2879789a5eb9228bd98a
Vinícius Gabriel <68403363+Vinicius7Dev@users.noreply.github.com>
2022-02-04 15:21:22 -0300

    delete order and your assessment in same request

--- backend/src/modules/orders/services/DeleteOrderService.ts
+++ backend/src/modules/orders/services/DeleteOrderService.ts
@@ -3,6 +3,7 @@ import { inject, injectable } from 'tsyringe';
 import AppError from '../../../shared/errors/AppError';
 import IOrdersRepository from '../repositories/IOrdersRepository';
 import IInstallationsRepository from '../../installations/repositories/IInstallationsRepository';
+import IAssessmentsRepository from '../../assessments/repositories/IAssessmentsRepository';
 
 @injectable()
 class DeleteOrderService {
@@ -12,6 +13,8 @@ class DeleteOrderService {
     private ordersRepository: IOrdersRepository,
     @inject('InstallationsRepository')
     private installationsRepository: IInstallationsRepository,
+    @inject('AssessmentsRepository')
+    private assessmentsRepository: IAssessmentsRepository,
   ) { }
 
   // Serviço para apagar um pedido
@@ -24,8 +27,13 @@ class DeleteOrderService {
     }
 
     // Verificando se o pedido tem alguma instalação associada
-    if (orderToDelete.installation && orderToDelete.installation.id) {
+    if (orderToDelete.installation) {
       await this.installationsRepository.delete(orderToDelete.installation.id);
+    }
+
+    // Verificando se o pedido tem alguma avaliação de instalação associada
+    if (orderToDelete.installation && orderToDelete.installation.assessment) {
+      await this.assessmentsRepository.delete(orderToDelete.installation.assessment.id);
     }
 
     // Apagando o pedido
