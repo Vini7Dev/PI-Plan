147c3278c2f85d001da54f9e942813d93579a9e9
Vinícius Gabriel <68403363+Vinicius7Dev@users.noreply.github.com>
2022-02-04 15:24:56 -0300

    delete installation and your assessment in the same request

--- backend/src/modules/installations/services/DeleteInstallationService.ts
+++ backend/src/modules/installations/services/DeleteInstallationService.ts
@@ -2,6 +2,7 @@ import { inject, injectable } from 'tsyringe';
 
 import AppError from '../../../shared/errors/AppError';
 import IInstallationsRepository from '../repositories/IInstallationsRepository';
+import IAssessmentsRepository from '../../assessments/repositories/IAssessmentsRepository';
 
 @injectable()
 class DeleteInstallationService {
@@ -9,7 +10,9 @@ class DeleteInstallationService {
     // Repositório das Instalações
     @inject('InstallationsRepository')
     private installationsRepository: IInstallationsRepository,
-  ) {}
+    @inject('AssessmentsRepository')
+    private assessmentsRepository: IAssessmentsRepository,
+  ) { }
 
   public async execute(id: string): Promise<string> {
     // Verificando se a instalação existe
@@ -17,6 +20,11 @@ class DeleteInstallationService {
 
     if (!installationToDelete) {
       throw new AppError('Installation not found.', 404);
+    }
+
+    // Verificando se a instalação tem alguma avaliação associada
+    if (installationToDelete.assessment) {
+      await this.assessmentsRepository.delete(installationToDelete.assessment.id);
     }
 
     // Executando o método para apagar uma instalação
