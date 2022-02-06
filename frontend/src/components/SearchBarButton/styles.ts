559bc3adb993d80edb04a894d246646893785118
Vinícius Gabriel <68403363+Vinicius7Dev@users.noreply.github.com>
2022-02-06 12:18:32 -0300

    Search bar style fix - button align

--- frontend/src/components/SearchBarButton/styles.ts
+++ frontend/src/components/SearchBarButton/styles.ts
@@ -14,28 +14,29 @@ export const Container = styled.div<IContainerProps>`
       width: 80px;
       height: 65px;
       margin-left: 20px;
+      margin-bottom: 10px;
       border: none;
       border-radius: 50%;
       box-shadow: 3px 3px 1px rgba(0, 0, 0, 0.2);
       transition: opacity 200ms;
 
       background-color: ${props => {
-        switch(props.color) {
-          case 'white':
-            return '#fffff';
-          default:
-            return '#91D2A1';
-        }
-      }};
+    switch (props.color) {
+      case 'white':
+        return '#fffff';
+      default:
+        return '#91D2A1';
+    }
+  }};
 
       color: ${props => {
-        switch(props.color) {
-          case 'white':
-            return '#ceaa7b';
-          default:
-            return '#ffffff';
-        }
-      }};
+    switch (props.color) {
+      case 'white':
+        return '#ceaa7b';
+      default:
+        return '#ffffff';
+    }
+  }};
 
       &:hover {
         opacity: 0.8;
